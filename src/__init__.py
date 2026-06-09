4f241268c9a0b0de0399429642b0746f7ab9e4b9
existencenumber <499913478@qq.com>
2026-06-09 16:37:30 +0800

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,14 @@
+"""
+Process Space Geometry - Dynamic Number Path Integral
+=====================================================
+Reference implementation for reproducing all numerical results
+reported in:
+  W. Xu, "Process Space Geometry: A First-Principles Derivation
+  of Fundamental Physical Constants" (2026)
 
+Author: Wanpeng Xu
+Contact: xwp499913478@gmail.com
+"""
+
+__version__ = "1.0.0"
+__author__ = "Wanpeng Xu"
